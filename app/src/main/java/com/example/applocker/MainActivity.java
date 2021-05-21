6db6b8ea9539b233fed4c68d2d7db70f3f3323ef
Pratik Patel <pmp2@illinois.edu>
2021-05-21 18:57:46 -0500

    Fixed second fragment to show installed apps

--- app/src/main/java/com/example/applocker/MainActivity.java
+++ app/src/main/java/com/example/applocker/MainActivity.java
@@ -1,17 +1,9 @@
 package com.example.applocker;
 
-import android.content.pm.PackageManager;
 import android.os.Bundle;
-
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.view.View;
-
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
-import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
--- app/src/main/java/com/example/applocker/SecondFragment.java
+++ app/src/main/java/com/example/applocker/SecondFragment.java
@@ -1,9 +1,13 @@
 package com.example.applocker;
 
+import android.app.Activity;
+import android.content.pm.ApplicationInfo;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -12,7 +16,7 @@ import androidx.navigation.fragment.NavHostFragment;
 import com.example.applocker.databinding.FragmentSecondBinding;
 
 public class SecondFragment extends Fragment {
-
+    private ListView mListAppInfo;
     private FragmentSecondBinding binding;
 
     @Override
@@ -20,6 +24,26 @@ public class SecondFragment extends Fragment {
             LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState
     ) {
+        super.onCreate(savedInstanceState);
+        super.getActivity().setContentView(R.layout.layout_main);
+
+        mListAppInfo = (ListView)super.getActivity().findViewById(R.id.lvApps);
+        // create new adapter
+        AppInfoAdapter adapter = new AppInfoAdapter(this.getContext(), Utilities.getInstalledApplication(this.getContext()), super.getActivity().getPackageManager());
+        // set adapter to list view
+        mListAppInfo.setAdapter(adapter);
+        // implement event when an item on list view is selected
+        mListAppInfo.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView parent, View view, int pos, long id) {
+                // get the list adapter
+                AppInfoAdapter appInfoAdapter = (AppInfoAdapter)parent.getAdapter();
+                // get selected item on the list
+                ApplicationInfo appInfo = (ApplicationInfo)appInfoAdapter.getItem(pos);
+                // launch the selected application
+                Utilities.launchApp(parent.getContext(), SecondFragment.super.getActivity().getPackageManager(), appInfo.packageName);
+            }
+        });
 
         binding = FragmentSecondBinding.inflate(inflater, container, false);
         return binding.getRoot();
@@ -30,7 +54,7 @@ public class SecondFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
         binding.buttonSecond.setOnClickListener(new View.OnClickListener() {
-            @Override
+                @Override
             public void onClick(View view) {
                 NavHostFragment.findNavController(SecondFragment.this)
                         .navigate(R.id.action_SecondFragment_to_FirstFragment);
